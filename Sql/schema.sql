e497d8f90b1ea0fdbd3a0fc3131e4e25b1ff7449
albertomontilla17 <81578116+albertomontilla17@users.noreply.github.com>
2021-09-29 18:46:06 -0400

    BITCOIN Table and Schema of Database

---
+++ Sql/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "BITCOIN" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close*" decimal   NOT NULL,
+    "AdjClose**" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
