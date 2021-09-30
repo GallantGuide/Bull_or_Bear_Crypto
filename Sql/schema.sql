3c1391b00c36d5d25fc63ca9d941bbf5d2d6f062
albertomontilla17 <81578116+albertomontilla17@users.noreply.github.com>
2021-09-30 19:46:46 -0400

    Adding CARDANO + updated schema

--- Sql/schema.sql
+++ Sql/schema.sql
@@ -1,9 +1,29 @@
-CREATE TABLE "BITCOIN" (
+CREATE TABLE "BITCOIN (BTC)" (
     "Date" date   NOT NULL,
     "Open" decimal   NOT NULL,
     "High" decimal   NOT NULL,
     "Low" decimal   NOT NULL,
-    "Close*" decimal   NOT NULL,
-    "AdjClose**" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "ETHEREUM (ETH)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "CARDANO (ADA)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
     "Volume" decimal   NOT NULL
 );
