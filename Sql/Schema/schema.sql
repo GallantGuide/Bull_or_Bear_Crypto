d27d0cbcbd513742e21d6608f5b13466d0be2daf
albertomontilla17 <montillalberto@icloud.com>
2021-10-05 16:47:40 -0400

    newer schema

--- Sql/Schema/schema.sql
+++ Sql/Schema/schema.sql
@@ -27,3 +27,93 @@ CREATE TABLE "CARDANO (ADA)" (
     "AdjClose" decimal   NOT NULL,
     "Volume" decimal   NOT NULL
 );
+
+CREATE TABLE "COSMOS (ATOM1)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "BINANCE (BNB)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "DOGECOIN (DOGE)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "POLKADOT (DOT1)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "ETHEREUM CLASSIC (ETC)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "CHAINLINK (LINK)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "LITECOIN (LTC)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "SOLANO (SOL1)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
+
+CREATE TABLE "RIPPLE (XRP)" (
+    "Date" date   NOT NULL,
+    "Open" decimal   NOT NULL,
+    "High" decimal   NOT NULL,
+    "Low" decimal   NOT NULL,
+    "Close" decimal   NOT NULL,
+    "AdjClose" decimal   NOT NULL,
+    "Volume" decimal   NOT NULL
+);
