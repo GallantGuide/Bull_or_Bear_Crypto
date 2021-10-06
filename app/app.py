a4eadaa612984ee74b91aa82c54391777c9cafd3
Anthony.lopez011 <anthony.lopez011@gmail.com>
2021-10-06 15:37:42 -0400

    Hiroku TS part 3

--- app/app.py
+++ app/app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import functions
 import uuid
 
 app = Flask(__name__)
@@ -29,15 +28,41 @@ def test_template():
     request_type = request.method
     if request_type == 'GET':
         return render_template('index.html', href='static/Base_image.svg')
-    if request_type == 'POST':
+    else:
         text = request.form['text']
         random_string = uuid.uuid4().hex
         file = 'app/static/AgesAndHeights.pkl'
         model = load('app/test_model.joblib')
-        user_input = functions.user_input_np_arr(text)
+        user_input = user_input_np_arr(text)
         path = 'app/static/predictions' + random_string + '.svg'
-        functions.make_picture(file, model, user_input, path)
+        make_picture(file, model, user_input, path)
         return render_template('index.html', href=path[4:])
+
+def make_picture(training_data_fname, model, user_input_np_arr, output_file):
+  data = pd.read_pickle(training_data_fname)
+  data = data[data['Age'] > 0 ]
+  ages = data['Age']
+  heights = data['Height']
+  x_new = np.array(list(range(19))).reshape(19,1)
+  preds = model.predict(x_new)
+  fig= px.scatter(x = ages, y = heights, title= "Height V Age of People", labels = {'x':'Age (Years)', 'y': 'Heights(inches)'})
+  fig.add_trace(go.Scatter(x=x_new.reshape(19), y=preds, mode = 'lines', name = 'Model'))
+
+  new_preds = model.predict(user_input_np_arr)
+  fig.add_trace(go.Scatter(x=user_input_np_arr.reshape(len(user_input_np_arrl)), y = new_preds, name='New Outputs', mode ='markers', marker=dict(color ='green', size = 20, line=dict(color ='orange',width=2))))
+
+  fig.write_image(output_file, width = 800, engine='kaleido')
+  fig.show()
+
+def user_input_np_arr(float_str):
+  def is_float(s):
+    try:
+      float(s)
+      return True 
+    except:
+      return False  
+  floats = np.array([float(x) for x in float_str.split(',') if is_float(x)])
+  return floats.reshape(len(floats),1)    
 
 
 @app.route("/test_model")
@@ -52,4 +77,5 @@ def test_model():
 @app.route("/hello_world")
 def hello_world():
     return "Hello, World!"
-    
+
+
