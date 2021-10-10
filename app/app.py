8d4f6a533fe92a46dcf97f9fc59c343cbbf65820
Anthony Lopez <anthony.lopez011@gmail.com>
2021-10-10 13:11:28 -0400

    Updating redirect code

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from joblib import load
 from functions import make_picture, user_input_np_arr
+# heroku name convention from .functions import make_picture, user_input_np_arr
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -9,16 +10,23 @@ import uuid
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET','POST'])
 def welcome():
-    return render_template('welcome.html')
+    request_type = request.method
+    if request_type == "POST":
+        url_param = request.form["url"]
+        return redirect(url_for("url_param",param=url_param))
+    else:    
+        return render_template("welcome.html")
 
+@app.route("/<param>")
+def url_param(param):
+    return f'<h1> {param} <h1>'
 
+# heroku site settings 
 @app.route("/site", methods = ['GET','POST'])
 def site_template():
     request_type = request.method
-    if request_type == 'GET':
-        return render_template('index.html', href='static/Base_image.svg')
     if request_type == 'POST':
         text = request.form['text']
         random_string = uuid.uuid4().hex
@@ -28,14 +36,16 @@ def site_template():
         path = 'app/static/' + random_string + '.svg'
         make_picture(file, model, user_input, path)
         return render_template('index.html', href=path[4:])
+    else:
+        return render_template('index.html', href='static/Base_image.svg')    
 
+
+# Local test site settings 
 @app.route("/test_site", methods = ['GET','POST'])
 def test_template():
     request_type = request.method
-    if request_type == 'GET':
-        return render_template('index.html', href='static/Base_image.svg')
     if request_type == 'POST':
-        text = request.form['text']
+        text = request.form['data']
         random_string = uuid.uuid4().hex
         file = 'static/AgesAndHeights.pkl'
         model = load('test_model.joblib')
@@ -43,9 +53,12 @@ def test_template():
         path = 'static/' + random_string + '.svg'
         make_picture(file, model, user_input, path)
         return render_template('index.html', href=path)
+    else:
+        return render_template('index.html', href='static/Base_image.svg') 
 
 
-@app.route("/test_model")
+
+@app.route("/<test_model>")
 def test_model():
     test_np_input = np.array([[1],[2],[17]])
     model = load('app/test_model.joblib')
@@ -58,5 +71,8 @@ def test_model():
 def hello_world():
     return "Hello, World!"
 
+
+
+# Local site app config 
 if __name__ == "__main__":
     app.run(debug=True)
