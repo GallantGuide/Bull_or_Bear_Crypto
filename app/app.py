b71e5ba96f3b754256a6e8d2c84e545e48b4876d
Anthony Lopez <anthony.lopez011@gmail.com>
2021-10-14 16:36:31 -0400

    Heroku Troubleshooting 3

--- app/app.py
+++ app/app.py
@@ -3,8 +3,8 @@ from sqlalchemy import Table, MetaData
 from flask_sqlalchemy import SQLAlchemy
 from joblib import load
 from typing import DefaultDict
-from functions import make_picture, user_input_np_arr, make_bitpict
-# heroku name convention from .functions import make_picture, user_input_np_arr, make_bitpict
+#from functions import make_picture, user_input_np_arr, make_bitpict
+from .functions import make_picture, user_input_np_arr, make_bitpict
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -70,7 +70,7 @@ def Bitcoin_Image():
         text = request.form['data']
         random_string = uuid.uuid4().hex
         user_input = user_input_np_arr(text)
-        path = 'static/' + random_string + '.svg'
+        path = 'app/static/' + random_string + '.svg'
         make_bitpict(user_input, path)
         return render_template('bitcoin.html', href=path)
     else:
@@ -98,20 +98,20 @@ def site_template():
 
 
 # Local test site settings 
-@app.route("/test_site", methods = ['GET','POST'])
-def test_template():
-    request_type = request.method
-    if request_type == 'POST':
-        text = request.form['data']
-        random_string = uuid.uuid4().hex
-        file = 'static/AgesAndHeights.pkl'
-        model = load('test_model.joblib')
-        user_input = user_input_np_arr(text)
-        path = 'static/uuid/' + random_string + '.svg'
-        make_picture(file, model, user_input, path)
-        return render_template('index.html', href=path)
-    else:
-        return render_template('index.html', href='static/Base_image.svg') 
+# @app.route("/test_site", methods = ['GET','POST'])
+# def test_template():
+#     request_type = request.method
+#     if request_type == 'POST':
+#         text = request.form['data']
+#         random_string = uuid.uuid4().hex
+#         file = 'static/AgesAndHeights.pkl'
+#         model = load('test_model.joblib')
+#         user_input = user_input_np_arr(text)
+#         path = 'static/uuid/' + random_string + '.svg'
+#         make_picture(file, model, user_input, path)
+#         return render_template('index.html', href=path)
+#     else:
+#         return render_template('index.html', href='static/Base_image.svg') 
 
 
 @app.route("/index_base", methods = ['GET','POST'])
@@ -121,7 +121,7 @@ def JS_testing():
 @app.route("/test_model")
 def test_model():
     test_np_input = np.array([[1],[2],[17]])
-    model = load('test_model.joblib')
+    model = load('app/test_model.joblib')
     preds = model.predict(test_np_input)
     preds_str = str(preds)
     return preds_str 
