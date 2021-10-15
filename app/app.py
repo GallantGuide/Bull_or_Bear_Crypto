5c1bc1d8e616990f2b1160cbf7b8c46a91bf5a18
Anthony Lopez <anthony.lopez011@gmail.com>
2021-10-15 11:53:13 -0400

    other database files

--- app/app.py
+++ app/app.py
@@ -23,40 +23,34 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_path
 db = SQLAlchemy(app)
 
 K_Bitcoin = db.Table('K_BITCOIN', db.metadata, autoload=True, autoload_with=db.engine)
-
-# db = create_engine(db_path)
-
-
-@app.route("/ste")
-def welcome():
-    return(
-    '''
-    Welcome to Bull or Bear Crypto '\n
-    Available Routes: \n
-    /Site \n
-    /test_model \n
-    /hello_world \n
-    /api/v1.0
-    '''
-    )
+K_Cardano = db.Table('K_CARDANO', db.metadata, autoload=True, autoload_with=db.engine)
+K_Ethereum = db.Table('K_ETHEREUM', db.metadata, autoload=True, autoload_with=db.engine)
+# Ethereum = db.Table('K_BITCOIN', db.metadata, autoload=True, autoload_with=db.engine)
+# Cardnamo = db.Table('K_BITCOIN', db.metadata, autoload=True, autoload_with=db.engine)
 
 
+# Default App Route 
 @app.route("/", methods = ['GET','POST'])
 def test_template():
     request_type = request.method
-    if request_type == 'GET':
-        return render_template('index.html', href='static/images/Base_image.svg')
+    if request_type == 'POST':
+        return render_template('index.html')
     else:
-        text = request.form['text']
-        random_string = uuid.uuid4().hex
-        file = 'app/static/AgesAndHeights.pkl'
-        model = load('app/test_model.joblib')
-        user_input = user_input_np_arr(text)
-        path = 'app/static/predictions' + random_string + '.svg'
-        make_picture(file, model, user_input, path)
-        return render_template('index.html', href=path[4:])
+        return render_template('index.html')   
 
+@app.route("/<param>")
+def url_param(param):
+    return f'<h1> {param} <h1>'
 
+@app.route("/test_model")
+def test_model():
+    test_np_input = np.array([[1],[2],[17]])
+    model = load('test_model.joblib')
+    preds = model.predict(test_np_input)
+    preds_str = str(preds)
+    return preds_str
+
+# Images and plots
 @app.route("/bitcoin", methods = ['GET','POST'])
 def Bitcoin_Image():
     request_type = request.method
@@ -68,8 +62,27 @@ def Bitcoin_Image():
         make_picture(user_input, path)
         return render_template('bitcoin.html', href=path[4:])
     else:
-        return render_template('bitcoin.html', href='static/images/actual_vs_predictions.svg') 
+        return render_template('bitcoin.html', href='static/images/actual_vs_predictions.svg')       
 
+
+@app.route("/site", methods = ['GET','POST'])
+def test_template():
+    request_type = request.method
+    if request_type == 'POST':
+        text = request.form['text']
+        random_string = uuid.uuid4().hex
+        file = 'app/static/AgesAndHeights.pkl'
+        model = load('app/test_model.joblib')
+        user_input = user_input_np_arr(text)
+        path = 'app/static/predictions' + random_string + '.svg'
+        make_picture(file, model, user_input, path)
+        return render_template('site.html', href=path[4:])
+    else:
+        return render_template('site.html', href='static/images/Base_image.svg') 
+
+
+
+# Database connector app routes
 @app.route("/bitcoin_db", methods = ['GET','POST'])
 def Bitcoin_Search():
     request_type = request.method
@@ -77,19 +90,22 @@ def Bitcoin_Search():
         return "You clicked a button"
     else:
         k_bitcoin = db.session.query(K_Bitcoin).all()
-        return render_template('bitcoin_db.html', k_bitcoin=k_bitcoin)        
+        return render_template('bitcoin_db.html', k_bitcoin=k_bitcoin)         
 
-@app.route("/test_model")
-def test_model():
-    test_np_input = np.array([[1],[2],[17]])
-    model = load('test_model.joblib')
-    preds = model.predict(test_np_input)
-    preds_str = str(preds)
-    return preds_str
-
-
-@app.route("/hello_world")
-def hello_world():
-    return "Hello, World!"
-
-
+@app.route("/cardano_db", methods = ['GET','POST'])
+def Cardano_Search():
+    request_type = request.method
+    if request_type == 'POST':
+        return "You clicked a button"
+    else:
+        k_cardano = db.session.query(K_Cardano).all()
+        return render_template('cardano_db.html', k_cardano=k_cardano)
+        
+@app.route("/ethereum_db", methods = ['GET','POST'])
+def Ethereum_Search():
+    request_type = request.method
+    if request_type == 'POST':
+        return "You clicked a button"
+    else:
+        k_ethereum = db.session.query(K_Ethereum).all()
+        return render_template('ethereum_db.html', k_ethereum=k_ethereum)
